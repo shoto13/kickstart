b2e83331fa9374a1f1a69d4542a56da58643df61
shoto13 <wilsonrc@pm.me>
2022-09-04 18:06:47 +0100

    updates to move between pages with routes

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,15 +3,19 @@ import {Form, Button, Input, Message} from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import {Router} from '../../routes';
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
-    errorMessage: ''
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
+
+    this.setState({ loading:true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -20,9 +24,14 @@ class CampaignNew extends Component {
         .send({
           from: accounts[0]
         });
+
+      Router.pushRoute('/');
+
     } catch (err) {
       this.setState({errorMessage: err.message});
     }
+
+    this.setState({loading:false});
   };
 
 
@@ -45,7 +54,7 @@ class CampaignNew extends Component {
           </div>
 
           <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button
+          <Button loading={this.state.loading}
             content="Create Campaign"
             primary
           />
