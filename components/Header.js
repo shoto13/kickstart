2ecb891b44fffbb811c3d3d11d861566c1d1b396
rory <wilsonrc@pm.me>
2022-09-04 23:21:00 +0100

    Routing fixes to allow site traversal through headerbar Unique campaign path creationg

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
+import {Link} from '../routes';
 
 export default () => {
   return (
     <div style={{marginTop: '10px'}}  class="ui menu">
-      <a class="item">KickStart</a>
+
+      <Link route="/">
+        <a class="item">KickStarter</a>
+      </Link>
+
       <div class="right menu">
-        <a class="item">Campaigns</a>
-        <a class="item">+</a>
+
+        <Link route="/">
+          <a class="item">Campaigns</a>
+        </Link>
+        <Link route="/campaigns/new">
+          <a class="item">+</a>
+        </Link>
+
       </div>
     </div>
   )
--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
 const routes = require('next-routes')();
 
+routes
+  .add('/campaigns/new', '/campaigns/new')
+  //Wildcard ":address" allows each created campaign to get a unique path
+  .add('/campaigns/:address', '/campaigns/show');
+
 module.exports = routes;
