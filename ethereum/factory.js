bcb5fa501a6be7a60d22ecf96ebffd6ad5d0fe00
shoto13 <wilsonrc@pm.me>
2022-09-05 16:59:26 +0100

    Updates to campaign display
    - Updates to show individual page for each campaign, with cards 
    displaying campaign characteristics

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x8CFbF2d6C0f4C467bd78CbBFa24e0dEdcf3Ab7C1'
+  '0x5A3D043Db56Bf5B446c51aDa2b2e3CE77B18b986'
 );
 
 export default instance;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,78 @@
 import React, {Component} from 'react';
+import {Card} from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
+
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    return{
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
+  renderCards() {
+
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Campaign Manager',
+        description: 'The Manager created this campaign and can create requests on this campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution Required (wei)',
+        description: 'The minimum amount of wei required to contribute to this campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description: 'A Request is an attempt to withdraw funds from the Campaign to service the project'
+      },
+      {
+        header: approversCount,
+        meta: 'Number of approvers',
+        description: 'The number of individuals who have already donated to this campaign'
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign balance (Ether)',
+        description: 'The balance of the Campaign which is available for use'
+
+      }
+    ];
+
+  return <Card.Group items={items} />;
+  }
+
+
   render () {
-    return <h3>Campaign Show</h3>;
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+        {this.renderCards()}
+        <ContributeForm />
+      </Layout>
+    );
   }
 }
 
