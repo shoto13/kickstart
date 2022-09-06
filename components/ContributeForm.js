f679af6e899bf9231a0d9f2519ab70f284e23c9b
shoto13 <wilsonrc@pm.me>
2022-09-06 16:57:54 +0100

    Show campaign requests and create a request
    -Updates to campaign display including a new request form and front end 
    to show the current requests
    
    - added route mappings

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import {Form, Input, Message, Button} from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import {Router} from '../routes';
 
 class ContributeForm extends Component {
   state = {
-    value: ''
+    value: '',
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async event => {
@@ -13,22 +16,27 @@ class ContributeForm extends Component {
 
     const campaign = Campaign(this.props.address);
 
+    this.setState({loading: true, errorMessage: ''});
+
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.contribute().send({
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, 'ether')
       });
-    } catch (err) {
 
+      Router.replaceRoute(`/campaigns/${this.props.address}`)
+    } catch (err) {
+      this.setState({errorMessage: err.message});
     }
 
+    this.setState({loading: false, value: ''});
   };
 
 
   render () {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -38,7 +46,9 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>
+
+        <Message error header="OOOOOps!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
           Contribute
         </Button>
 
